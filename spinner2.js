8b60ee138f23e50625fb20fb06990fbf80a16407
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-06 11:40:57 -0700

    update spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,4 +1,4 @@
-const spinner = ["|", "/", "-", "\\"];
+const spinner = ["|", "/", "-", "\\", "|"];
 let delay = 50;
 
 for (let i = 0; i < spinner.length; i++) {
@@ -9,45 +9,22 @@ for (let i = 0; i < spinner.length; i++) {
     
   }, delay);
   delay += 100;
-  console.log();
+
 }
 
-// setTimeout(() => {
-//   process.stdout.write(`\r|  `);
-// }, 100);
 
-// setTimeout(() => {
-//   process.stdout.write(`\r/  `);
-// }, 300);
+// const spinner = ["|", "/", "-", "\\", "|"];
+// let delay = 100;
 
-// setTimeout(() => {
-//   process.stdout.write(`\r-  `);
-// }, 500);
-
-// setTimeout(() => {
-//   process.stdout.write(`\r\\  `);
+// for (let i = 0; i < spinner.length; i++) {
+//   setTimeout(() => {
+   
+//     process.stdout.write(spinner[i]);
   
-// }, 700);
+//   }, delay);
+//   delay += 100;
+//}
 
-// setTimeout(() => {
-//   process.stdout.write(`\r|  `);
-// }, 800);
-// setTimeout(() => {
-//   process.stdout.write(`\r/  `);
-// }, 900);
-
-// setTimeout(() => {
-//   process.stdout.write(`\r-  `);
-// }, 1000);
-
-// setTimeout(() => {
-//   process.stdout.write(`\r\\  `);
-  
-// }, 1100);
-
-// setTimeout(() => {
-//   process.stdout.write(`\r|  `);
-// }, 1200);
-// setTimeout(() => {
-//   process.stdout.write(`\n`);
-// }, 1300);
+setTimeout(() => { // new line
+  process.stdout.write(`\n`);
+}, delay);
